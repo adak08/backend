1cf59c30b515cc15d7e73889915a2c61e2f84e7d
adak08 <adarshkumarpbh1@gmail.com>
2025-08-12 00:35:01 +0530

    first

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 const express = require('express');
-const app = express();
+const app = express()
 
 
-const port = 4000
+const port = 4000;
 
 app.get('/', (req, res) => {
   res.send('Hello fucking world!')
